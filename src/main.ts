72e250a6814402afcde035945cdae8f14da0e817
Andelf <andelf@gmail.com>
2021-12-01 17:35:19 +0800

    ci

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
-import * as glob from '@actions/glob'
+// import * as github from '@actions/github'
+// import * as glob from '@actions/glob'
+// import { GitHub } from "@actions/github/lib/utils";
 
-import {wait} from './wait'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+    const tagName: string = core.getInput('tag_name')
+    core.notice(`this tag name is ${tagName}`)
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
