ed6088ee9682683b273ebdba5199f1ef6185a70e
Andelf <andelf@gmail.com>
2021-12-02 10:42:03 +0800

    *: save

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import * as core from '@actions/core'
 // import * as github from '@actions/github'
 // import * as glob from '@actions/glob'
-// import { GitHub } from "@actions/github/lib/utils";
+import { GitHub } from '@actions/github/lib/utils'
+// import { context } from '@actions/github'
 
 import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
+    const github = new GitHub({ auth: process.env.GITHUB_TOKEN });
+
+    const rel = await github.rest.repos.getReleaseByTag({
+      owner: 'andelf',
+      repo: 'nightly-release',
+      tag: 'nightly'
+    })
+
+    core.info(`got ${rel}`)
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
